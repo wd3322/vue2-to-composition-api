e51c11ffa399a5caf4ae69211aec181b003b61d3
wudi <wd87269288@163.com>
2023-02-20 15:21:44 +0800

    fix: replace key

--- src/main.ts
+++ src/main.ts
@@ -538,7 +538,7 @@ function Vue2ToCompositionApi(
                 const content: string = contents[i]
                 const key: string = content.substring(0, Math.min(
                   ...utilMethods.getIndexArr({
-                    values: ['\n', '\t', ' ', '.', ',', ';', '?', '[', ']', ')', '(', '+', '-'],
+                    values: ['\n', '\t', ' ', '.', ',', ';', '?', '[', ']', ')', '(', '+', '-', '{', '}'],
                     content,
                     start: 0,
                     append: false
